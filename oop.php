9c71cf415d4dc6b1d21466c6fefbffa0cef08794
Adnan faruque Alvi <17103169@iubat.edu>
2020-09-19 14:31:01 +0600

    third commit

--- oop.php
+++ oop.php
@@ -9,9 +9,11 @@ class Mobile
        $model = $number;
        echo "Model Number: $model";
    } 
-   
-function wegufuw(){
-     echo "adnan";
-  }
 }
+
+$samsung = new Mobile;
+$samsung->showModel('A8');
+echo "<br>";
+$lg = new Mobile;
+$lg->showModel('lg G4');
 ?>
